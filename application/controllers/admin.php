18677034fe9685ced1629dfa94adb953a9fd6e6d
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-04-19 02:01:42 +0700

    melakukan penambahan pada controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,7 +2,12 @@
 
 class Admin extends CI_Controller {
     public function dashboard(){
-
+        $this->load->view('template_admin/header');
+        $this->load->view('template_admin/sidebar');
+        $this->load->view('template_admin/topnav');
+        $this->load->view('admin/dashboard');
+        $this->load->view('template_admin/footer');
+        
 
     }
 
